294921faccdaf312f1e240230d5bf4838cde8eda
YuepingShan <166201751+YuepingShan@users.noreply.github.com>
2025-06-02 11:25:48 +0800

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 const nextConfig = {
   output: 'export', // 关键！启用静态导出
-  distDir: '/app/output',
+  distDir: '/root/cloudbase-workspace/app/output',
   images: {
     unoptimized: true // 禁用图片优化
   }
